6969e72e6604f7520bcad11bcc0df865c6cd9e93
Tom <zuckm709@gmail.com>
2026-05-23 06:23:35 -0500

    ...

--- backend/services/fez.js
+++ backend/services/fez.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 // services/fezAuth.ts
 
-const FEZ_BASE = "https://b2b-dev.fezdelivery.co/";
+const FEZ_BASE = "https://b2b-dev.fezdelivery.co";
 //  "https://api.fezdelivery.co/";
 
 let fezToken = null;
