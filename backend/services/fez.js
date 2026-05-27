276f6c906979733cf382791c9ddf90c1f3491e86
Tom <zuckm709@gmail.com>
2026-05-27 08:06:38 -0500

    test

--- backend/services/fez.js
+++ backend/services/fez.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const FEZ_BASE = "https://api.fezdelivery.co";
 
-// "https://apisandbox.fezdelivery.co/v1";
+// const FEZ_BASE = "https://apisandbox.fezdelivery.co/v1";
 
 let fezToken = null;
 let fezSecretKey = null;
