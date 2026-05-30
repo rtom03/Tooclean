4720d1df1b9d1d493c793b0fb9e91e01e2007a5b
Tom <zuckm709@gmail.com>
2026-05-30 05:49:17 -0500

    implement optim

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,7 +1,7 @@
 export const generateDiscountCode = (name) => {
   const cleanedName = name.replace(/\s+/g, "").toUpperCase();
 
-  const random = Math.random().toString(36).substring(2, 6).toUpperCase();
+  // const random = Math.random().toString(36).substring(2, 6).toUpperCase();
 
-  return `${cleanedName}-${random}`;
+  return `${cleanedName}`;
 };
