d3a61b157ea76aa77e709ea96ef6f4ec691c4776
Tom <zuckm709@gmail.com>
2026-09-04 15:57:33 +0100

    ..

--- frontend/src/components/TopSellers.tsx
+++ frontend/src/components/TopSellers.tsx
@@ -17,26 +17,28 @@ const TopSellers = () => {
         SHOP HERE
       </h1>
       <div className="grid grid-cols-2 gap-4 w-full max-w-2xl text-[#FBF6F0]">
-        {data.products.map((p) => (
-          <Link to={`/product/${p.id}`} key={p.id}>
-            <div className="bg-[#453224] rounded-2xl overflow-hidden aspect-3/4">
-              <img
-                src={p.images[0]}
-                alt={`Too Clean ${p.name}`}
-                className="w-full h-full object-cover object-center"
-              />
-            </div>
-            <div className="pt-4 px-1 ">
-              <p className="text-[11px] font-bold tracking-[0.06em] uppercase ">
-                {p.name}
-              </p>
-              <p className="text-[12px]  mt-0.5">{p.color}</p>
-              <p className="text-[15px] font-semibold  mt-2">
-                ₦{p.price.toLocaleString("en-NG")}
-              </p>
-            </div>
-          </Link>
-        ))}
+        {data.products
+          .filter((prod) => prod.isActive)
+          .map((p) => (
+            <Link to={`/product/${p.id}`} key={p.id}>
+              <div className="bg-[#453224] rounded-2xl overflow-hidden aspect-3/4">
+                <img
+                  src={p.images[0]}
+                  alt={`Too Clean ${p.name}`}
+                  className="w-full h-full object-cover object-center"
+                />
+              </div>
+              <div className="pt-4 px-1 ">
+                <p className="text-[11px] font-bold tracking-[0.06em] uppercase ">
+                  {p.name}
+                </p>
+                <p className="text-[12px]  mt-0.5">{p.color}</p>
+                <p className="text-[15px] font-semibold  mt-2">
+                  ₦{p.price.toLocaleString("en-NG")}
+                </p>
+              </div>
+            </Link>
+          ))}
       </div>
     </section>
   );
